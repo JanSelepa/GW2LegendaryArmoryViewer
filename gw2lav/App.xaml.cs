4e1bead2ee4d8645ff766ab590685393385fe81f
JanSelepa <jan.selepa@gmail.com>
2022-09-08 14:19:52 +0000

    single exe build

--- gw2lav/App.xaml.cs
+++ gw2lav/App.xaml.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
 using System.Windows;
 
 namespace gw2lav {
@@ -11,5 +9,29 @@ namespace gw2lav {
 	/// Interaction logic for App.xaml
 	/// </summary>
 	public partial class App : Application {
+
+		public App() {
+			AppDomain.CurrentDomain.AssemblyResolve += OnResolveAssembly;
+		}
+
+		/*
+		 * Used for embedding *.dll into main .exe file
+		 */
+		private static Assembly OnResolveAssembly(object sender, ResolveEventArgs args) {
+			Assembly executingAssembly = Assembly.GetExecutingAssembly();
+			AssemblyName assemblyName = new AssemblyName(args.Name);
+
+			var path = assemblyName.Name + ".dll";
+			if (assemblyName.CultureInfo.Equals(CultureInfo.InvariantCulture) == false) path = String.Format(@"{0}\{1}", assemblyName.CultureInfo, path);
+
+			using (Stream stream = executingAssembly.GetManifestResourceStream(path)) {
+				if (stream == null) return null;
+
+				var assemblyRawBytes = new byte[stream.Length];
+				stream.Read(assemblyRawBytes, 0, assemblyRawBytes.Length);
+				return Assembly.Load(assemblyRawBytes);
+			}
+		}
+
 	}
 }
