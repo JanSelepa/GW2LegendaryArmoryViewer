b3f6aeace9c35185b80cecc5e8eff2bf69b45aa1
JanSelepa <jan.selepa@gmail.com>
2022-09-09 12:40:00 +0000

    custom app titlebar

--- gw2lav/View/MainWindow.xaml.cs
+++ gw2lav/View/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using gw2lav.ViewModel;
+using System;
 using System.Windows;
 
 namespace gw2lav.View {
@@ -11,12 +12,44 @@ namespace gw2lav.View {
 			DialogService dialogService = new DialogService(this);
 			dialogService.Register<SettingsViewModel, SettingsWindow>();
 
+			StateChanged += onStateChangedWindow;
+
 			DataContext = new LegendaryViewModel(dialogService);
 			InitializeComponent();
 		}
 
 		private async void OnLoaded(object sender, RoutedEventArgs e) {
 			await ((LegendaryViewModel)DataContext).LoadDataAsync();
+		}
+
+		private void onMinimizeClick(object sender, RoutedEventArgs e) {
+			WindowState = WindowState.Minimized;
+		}
+
+		private void onMaximizeClick(object sender, RoutedEventArgs e) {
+			WindowState = WindowState.Maximized;
+		}
+
+		private void onRestoreClick(object sender, RoutedEventArgs e) {
+			WindowState = WindowState.Normal;
+		}
+
+		private void onCloseClick(object sender, RoutedEventArgs e) {
+			Close();
+		}
+
+		private void onStateChangedWindow(object sender, EventArgs args) {
+			// fix for maximized window overlapping the screen
+			if (WindowState == WindowState.Maximized) {
+				double scale = PresentationSource.FromVisual((Window)sender).CompositionTarget.TransformToDevice.M11;
+				root.Margin = new Thickness(
+					(SystemParameters.WindowNonClientFrameThickness.Left + SystemParameters.WindowResizeBorderThickness.Left - 1) / scale/** 96d / (int)typeof(SystemParameters).GetProperty("DpiX", BindingFlags.NonPublic | BindingFlags.Static).GetValue(null, null)*/,
+					(SystemParameters.WindowNonClientFrameThickness.Top + SystemParameters.WindowResizeBorderThickness.Top - SystemParameters.CaptionHeight - 2) / scale,
+					(SystemParameters.WindowNonClientFrameThickness.Right + SystemParameters.WindowResizeBorderThickness.Right - 1) / scale,
+					(SystemParameters.WindowNonClientFrameThickness.Bottom + SystemParameters.WindowResizeBorderThickness.Bottom - 1) / scale
+				);
+			} else
+				root.Margin = new Thickness(0);
 		}
 
 	}
